f909b8efbb10b989f406889525e3511815e1c6ab
Rituparna <rituparna.june@gmail.com>
2020-09-29 10:04:02 +0200

    login form part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -15,11 +16,13 @@ import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import { DishService } from './services/dish.service';
+import { LeaderService } from './services/leader.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +33,8 @@ import { ContactComponent } from './contact/contact.component';
     FooterComponent,
     AboutComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -41,9 +45,14 @@ import { ContactComponent } from './contact/contact.component';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDialogModule
   ],
-  providers: [DishService],
+  providers: [DishService,
+    LeaderService],
+  entryComponents: [
+    LoginComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -13,11 +13,9 @@ export class DishService {
     return DISHES;
   }
 
-  getDish (id: string): Dish {
-
-    return DISHES.filter(dish => dish.id === id)[0];
-    
-    }
+  getDish(id: string): Dish {
+    return DISHES.filter((dish) => (dish.id  === id))[0];
+  }
 
   getFeaturedDish(): Dish {
     return DISHES.filter((dish) => dish.featured)[0];
